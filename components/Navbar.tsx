3143effc7a5ca4f22b1eda8795183e0540fa449a
Mrudula <mrudulareddy0329@gmail.com>
2026-02-12 09:33:52 +0000

    Updated Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -87,7 +87,7 @@ export default function Navbar() {
                 href="#contact" 
                 className="inline-flex items-center justify-center rounded-full px-4 min-[1440px]:px-5 min-[1920px]:px-8 py-2 text-[12px] min-[1440px]:text-[13px] min-[1920px]:text-[15px] border border-primary text-primary transition-all duration-300 hover:bg-primary hover:text-white h-[34px] min-[1920px]:h-[44px]"
               >
-                Contact now
+                Enquire now
               </Link>
             </div>
 
