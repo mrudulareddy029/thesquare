f5f655933fe288261fecd40564fb3a0e66def1b9
Mrudula <mrudulareddy0329@gmail.com>
2026-02-14 09:55:48 +0000

    update

--- components/WhatsAppWidget.tsx
+++ components/WhatsAppWidget.tsx
@@ -1,20 +1,19 @@
 "use client";
 
 import Script from "next/script";
-import { useEffect } from "react";
 
 export default function WhatsAppWidget() {
   return (
     <Script
       src="https://do8wl071qiuy9.cloudfront.net/integration-plugin.js"
       id="wa-widget"
-      strategy="afterInteractive"
-      // We use both 'widget-id' and 'data-widget-id' to be safe
-      {...{ "widget-id": "9JjuP9" }} 
-      data-widget-id="9JjuP9"
-      onLoad={() => {
-        console.log("WhatsApp Widget Script loaded successfully");
+      strategy="lazyOnload" // Loads later so it doesn't block mobile images
+      {...{ 
+        "widget-id": "9JjuP9",
+        "data-mobile": "true", // Some plugins require this for mobile
+        "data-position": "right" 
       }}
+      data-widget-id="9JjuP9"
     />
   );
 }
